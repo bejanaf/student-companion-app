574a4ae36acffed05fca754fb5e3f0c7890502a3
bejan afshar <cosmopolit_b@hotmail.de>
2021-04-30 20:09:15 +0200

    changed js-modules and build the buddy-cards on index git push

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 //Hier drunter wird Team 1 sein Projekt importieren
+/*
 import { sum, subtract } from './lib/teamBuddies';
 import { multiply, divide } from './lib/ratingHover';
-
-// mit default im export als ersten
-// import sum, { subtract, multiply, divide } from './lib/math';
-//import { PI as piNumber } from './lib/math';
-
+*/
+/*
 console.log(sum(4, 5));
 console.log(subtract(8, 4));
 console.log(multiply(6, 4));
 console.log(divide(8, 2));
-
-//Hier drunter wird Team 1 sein Projekt importieren
+*/
